09b85ba36a60c743ef673e7d00fea88cb743e9db
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-04-04 20:56:58 -0400

    complete exercise 2

--- python-functions.py
+++ python-functions.py
@@ -12,4 +12,16 @@ sum_to(10)
 # 2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 
 def largest(num_list):
-  
+  return max(num_list)
+
+largest([1, 2, 25, 4, 99])
+
+# 3. Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
+
+def occurences(string1, string2):
+  count = 0
+  if string1 in string2:
+    count += 1
+  print(count)
+
+occurences('fleep floop', 'e')
